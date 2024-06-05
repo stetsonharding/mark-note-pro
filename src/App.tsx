1e2ccee869f5c7ccca8d79c4fba403d20a065faf
stetson <stetson2001@msn.com>
2024-06-05 07:55:28 -0700

    Created Note and tags.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import {Routes,Route, Navigate} from 'react-router-dom'
+import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import {Container} from 'react-bootstrap'
-import NewNote from "./components/NewNote"
+import { Container } from "react-bootstrap";
+import NewNote from "./components/NewNote";
+import Dashboard from "./Dashboard";
+
+import { Note } from "./models/model";
 
 function App() {
 
+  //State to hold all created notes.
+  const [notes, setNotes] = useState<Note[]>([])
 
+  console.log(notes)
+  
   return (
- <Container className="my-4">
-
-<Routes>
-
-  <Route path="/" element={<h1>Home</h1>} />
-  <Route path="/newNote" element={<NewNote />} />
-  <Route path="/:id">
-    <Route index element={<h1>Show</h1>} />
-    <Route path="edit" element={<h1>edit</h1>} />
-  </Route>
-  <Route path="*" element={<Navigate to="/"/>}/>
-</Routes>
- </Container>
-  )
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/newNote" element={<NewNote setNotes={setNotes} />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/components/NewNoteForm.tsx
+++ src/components/NewNoteForm.tsx
@@ -1,28 +1,82 @@
-import React from "react";
-
+import React, {ChangeEvent, FormEvent, useRef, useState} from 'react'
 import { Form, Button, Stack, Row, Col } from "react-bootstrap";
 
+import Creatable from "react-select/creatable";
+import { Note} from '../models/model';
+import { Tag } from '../models/model';
+import { Link } from 'react-router-dom';
 
-function NewNoteForm() {
+type NoteFormProps = {
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+}
+
+const NewNoteForm = ({setNotes} : NoteFormProps) => {
+const [newNote, setNewNote] = useState<Note>({id: "", title: "", markdown:"", tags:[]})
+const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+
+const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+  e.preventDefault();
+  const { name, value } = e.target;
+
+  setNewNote(prevNotes => ({
+    ...prevNotes,
+    [name]: value,
+    tags: selectedTags
+  }));
+  
+};
+
+const glaks = (e:FormEvent) => {
+  e.preventDefault();
+setNotes(prev => [...prev, {...newNote, tags: selectedTags}])
+}
+
+
   return (
-<Form>
-    <Stack gap={4}>
+    <Form onSubmit={glaks}>
+      <Stack gap={4}>
         <Row>
-            <Col>
-           <Form.Group controlId="title">
-            <Form.Label>Title</Form.Label>
-            <Form.Control  required/>
-           </Form.Group>
-            </Col>
-            <Col>
-           <Form.Group controlId="tags">
-            <Form.Label>tags</Form.Label>
-            <Form.Select  required />
-           </Form.Group>
-            </Col>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder='Enter Note Title' name="title" onChange={handleOnChange} required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <Creatable value={selectedTags.map(tag =>{
+                return {label: tag.label, value: tag.id}
+              })} 
+              isMulti 
+              onChange={tags => {
+                setSelectedTags(tags.map(tag => {
+                  return {label: tag.label, id: tag.value}
+                }))
+              }}
+              />
+            </Form.Group>
+          </Col>
         </Row>
-    </Stack>
-</Form>
+        <Row>
+          <Col>
+            <Form.Group controlId="markdown">
+              <Form.Label>Body</Form.Label>
+              <Form.Control as="textarea" rows={15} name="markdown" onChange={handleOnChange} required />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Stack>
+      <Row>
+        <Col className="d-flex gap-2 mt-2 justify-content-end">
+          <Button type="submit">Save</Button>
+          <Link to="/">
+          <Button variant="light" type='button'>Cancel</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
